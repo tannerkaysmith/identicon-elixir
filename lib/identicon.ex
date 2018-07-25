5a6acc2d892b134fbd9d4689ad1fe6dac4ee270a
tannerkaysmith <34726916+tannerkaysmith@users.noreply.github.com>
2018-07-25 12:51:05 -0600

    Updating Structs

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,20 +1,18 @@
 defmodule Identicon do
- def main(input) do
-  input
-  |> hash_input
-  |> pick_color
- end
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
 
- def pick_color(image) do
-  %Identicon.Image{hex: [r, g, b | _tail]} = image
-  
-  [r, g, b]
- end
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
 
- def hash_input(input) do
-  hex = :crypto.hash(:md5, input)
-  |> :binary.bin_to_list
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
 
-  %Identicon.Image{hex: hex}
- end
+    %Identicon.Image{hex: hex}
+  end
 end
